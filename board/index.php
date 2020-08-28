e19b012d8145e197a1dc68e2a5b45080352bfc73
kyujin2008 <dadanoac@gmail.com>
2020-08-28 23:02:28 +0900

    백그라운드 추가

--- board/index.php
+++ board/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lank="ko">
 
 	<head>
 			
@@ -10,38 +10,47 @@
 	</head>
 		
 	<body>
-
-		<?php 
-			$index = 0;
-
-			$mysqli = mysqli_connect("localhost","test","test","tableDB");
-			if (mysqli_connect_errno())
-			{
-	  			echo "Failed to connect to MySQL". mysqli_connect_error();
-	  		}
-
-	  		$query = "SELECT * FROM tableBoard"; 
-
-			//$data = mysql_query($query) ; 
-
-			//$row = mysql_num_rows($result); 
-
-		?>
-
-		<table>
-		<?php
-			echo "<tr><td>"."index"."</td><td>"."title"."</td><td>"."name"."</td><td>"."date"."</td></tr>";
-			if($result = $mysqli->query($query))
-			{
-				while($row = $result->fetch_row()){
-					echo "<tr><td>".$row[0]."</td><td>"."<a href='read.php?no=$row[0]'>".$row[1]."</a></td><td>".$row[3]."</td><td>".$row[4]."</td></tr>";
-				}
-			}
-			
-	    ?>
-		</table>
-  	   	
-		<button onclick="location.href='newPost.php'">글쓰기</button>
 		
+		<div class="wrapper">
+			<div class="header">
+				<a class="topText" href="<?php echo 'index.php'?>">아이유짱</a>
+				<div class="headerLogo">
+					
+				</div>
+			</div>
+			<div class="content">
+
+				<?php 
+					$index = 0;
+
+					$mysqli = mysqli_connect("localhost","test","test","tableDB");
+					if (mysqli_connect_errno())
+					{
+			  			echo "Failed to connect to MySQL". mysqli_connect_error();
+			  		}
+
+			  		$query = "SELECT * FROM tableBoard"; 
+
+					//$data = mysql_query($query) ; 
+
+					//$row = mysql_num_rows($result); 
+
+				?>
+
+				<table>
+					<?php
+						echo "<tr><td>"."index"."</td><td>"."title"."</td><td>"."name"."</td><td>"."date"."</td></tr>";
+						if($result = $mysqli->query($query))
+						{
+							while($row = $result->fetch_row()){
+								echo "<tr><td>".$row[0]."</td><td>"."<a href='read.php?no=$row[0]'>".$row[1]."</a></td><td>".$row[3]."</td><td>".$row[4]."</td></tr>";
+							}
+						}
+						
+				    ?>
+				</table>
+				<button class="writeButton" onclick="location.href='newPost.php'">글쓰기</button>
+		  	</div>
+		</div>
 	</body>
 </html>
--- board/main.js
+++ board/main.js
@@ -4,17 +4,72 @@ const app = new Vue({
 	el: "#myApp",
 
 	data: {
-		jeff:{
-			name: "mr.황",
-			age:20
+		people: [
+
+			{
+				name: "mr.황",
+				nickName: "코드마스터",
+				age:20
+			},
+			{
+				name: "susan",
+				nickName: "코드좁밥",
+				age:21
+			},
+			
+			{ 
+				name: "질레이너",
+				nickName: "망나니",
+				age:35
+			}
+		]	
+	},
+
+	//요소에 접근이 가능하다.
+	computed:{
+/*		printJeffName()	{
+			return `${this.jeff.nickName} ${this.jeff.name}`
 		},
-		susan:{
-			name: "susan",
-			age:21
+
+		printSusanName(){
+			return `${this.susan.nickName} ${this.susan.name}`
 		},
-		jill: "질레이너"
+
+		printJillName()	{
+			return `${this.jill.nickName} ${this.jill.name}`
+		},	
+
+		jeffInOneYear(){
+			return this.jeff.age + 1;
+		}*/
 
 	},
+
+	filters:{
+		//이름을 필터링 -> input 사용이 가능한 computed인듯.
+		printName(inputValue) {
+			return `${inputValue.nickName} ${inputValue.name}`
+		},
+
+		inOneYear(inputValue) {
+			return inputValue.age + 1;
+
+		}
+	},
+
+	//함수를 정의
+	methods: {
+		//나이가 증가한다.
+		addAge(person){
+			person.age = person.age + 1;	
+		},
+
+		minusAge(person){
+			person.age = person.age - 1;
+		}
+		
+	},
+
 	beforeCreate(){
 		console.log("beforeCreate() called");
 	},
@@ -49,10 +104,22 @@ const app = new Vue({
 
 	template: `
 		<div>
-		<h2>이름: {{jeff.name}}</h2>
-		<h2>나이: {{jeff.age}}</h2>
-		<h2>친구1: {{jill}}</h2>
-		<h2>친구2: 이름:{{susan.name}}, 나이:{{susan.age}}</h2>
+
+			<h2 v-for="person in people">
+				<h4>
+					{{person | printName}}
+				</h4>
+
+				<h5>
+					{{person.age}}
+				</h5>
+				<input type="text" v-model="person.name"/>
+				<input type="text" v-model="person.nickName"/>
+				<br>
+					<button v-on:click='addAge(person)'>+</button>
+					<button v-on:click='minusAge(person)'>-</button>
+				<br>
+			</h2>
 		</div>
 	`
 
